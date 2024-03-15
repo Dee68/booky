2326902004644af4e7fd082b0ab1ee1dec52df55
dimie egberipou <ddimie283@gmail.com>
2024-03-15 00:27:46 +0000

    Add crud funtionality to category

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -16,5 +16,78 @@ namespace BookStore.Controllers
             List<Category> categoryList = _db.Categories.ToList();
             return View(categoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category successfully created.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound(); 
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category successfully updated.";
+                return RedirectToAction("Index");
+            }
+            
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            
+             Category? obj = _db.Categories.Find(id);
+             if(obj == null)
+             {
+                return NotFound(id);
+             }
+             _db.Categories.Remove(obj);
+             _db.SaveChanges();
+            TempData["success"] = "Category successfully deleted.";
+            return RedirectToAction("Index");
+            
+            
+        }
     }
 }
--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class Category
     {
         public int Id { get; set; }
-        [Required]
+        [Required, DisplayName("Category Name"), MaxLength(30)]
         public string? Name { get; set; }
+        [DisplayName("Display Order"), Range(1,100)]
         public int DisplayOrder { get; set; }
     }
 }
